4f73e8788fc35184072d144197f20ef999c8cd31
taylulz <taylulzcode@gmail.com>
2020-11-20 13:41:57 -0800

    add working fetch api to service logic

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,8 +1,8 @@
 // This function will fetch the echange rates in JSON format
 export default class ExchangeService {
-  static async getExchangeRates(baseCurrency) {
+  static async getExchangeRates() {
     try { 
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}/`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error(response.statusText);
       } 
