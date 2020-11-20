e633c2eee7b17660f7b428ddc6482f01f86e81f8
taylulz <taylulzcode@gmail.com>
2020-11-20 13:42:52 -0800

    add bare-minimum working api call response

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,29 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import '.././css/styles.css';
 import ExchangeService from './exchange-service.js';
-// import ImageService from './image-service.js';
-// import ImgSearch from './img-search.js';
+
+
+function getElements(response) {
+  if (response.conversion_rates) {
+    $('#results').text(`Conversion rate ${response.conversion_rates.USD}`);
+  } else {
+    $('#showErrors').text(`Something went wrong`);
+  }
+}
 
 async function getExchangeRates() {
   const response = await ExchangeService.getExchangeRates();
   getElements(response);
 }
 
-function getElements(response) {
-  if (response is working) {
-    $('#id').text(`${response.json.WhatToDisplay}`)
-  } else {
-    $('#errorId').text(`${response.message.fromError}`)
-  }
-}
-
 $(document).ready(function() {
-  event.preventDefault();
-  $('#searchBox').submit(function(event){
+  $('#btn-submit').click(function(event) {
+    event.preventDefault();
     getExchangeRates();
+
   });
 });
 
-// response.conversion_rates.{currency_code}
+// response.conversion_rates.currency_code
